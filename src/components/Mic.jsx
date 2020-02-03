2ab19c0b3addb5aa6e17c50b1564e4af5aaad512
Mahmoud Essam <mahmoudessam1456@gmail.com>
2020-02-03 19:36:22 +0200

    handle login

--- src/components/Mic.jsx
+++ src/components/Mic.jsx
@@ -1,11 +1,18 @@
 import { ReactMic } from "react-mic";
 import React, { Component } from "react";
+import http from "../services/httpServices";
 
 export default class Mic extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      record: false
+      record: false,
+      client: {
+        recordName: "",
+        ayah: "",
+        hokm: "",
+        record: ""
+      }
     };
   }
 
@@ -27,6 +34,18 @@ export default class Mic extends Component {
 
   onStop(recordedBlob) {
     console.log("recordedBlob is: ", recordedBlob);
+    console.log(recordedBlob.blob);
+    var data = new FormData();
+    data.append("filename", "ahmed32.wav");
+    data.append("record", recordedBlob.blob);
+    console.log(data.getAll);
+    var client = {
+      recordName: "ahmed32.wav",
+      ayah: "naran_thaat",
+      hokm: "Ekhfaa",
+      record: data
+    };
+    http.post("http://localhost:3000/api/client", client);
   }
 
   render() {
@@ -44,9 +63,11 @@ export default class Mic extends Component {
         <button onClick={this.startRecording} type="button">
           Start
         </button>
-        <button onClick={this.stopRecording} type="button">
-          Stop
-        </button>
+        <form encType="multipart/form-data" method="post" action="#" id="#">
+          <button onClick={this.stopRecording} type="button">
+            Stop
+          </button>
+        </form>
       </div>
     );
   }
