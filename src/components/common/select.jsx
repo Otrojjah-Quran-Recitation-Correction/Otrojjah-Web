ecfc4323e35c14a505c36fa84d20a22f7567b4a3
Mahmoud Essam <mahmoudessam1456@gmail.com>
2020-06-13 22:39:38 +0200

    select hover

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -5,7 +5,12 @@ const Select = ({ name, label, options, error, ...rest }) => {
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       {options[0] && (
-        <select name={name} id={name} {...rest} className="form-control">
+        <select
+          name={name}
+          id={name}
+          {...rest}
+          className="form-control custom-select"
+        >
           <option value="" />
           {options.map(option => (
             <option key={option._id} value={option[name]}>
